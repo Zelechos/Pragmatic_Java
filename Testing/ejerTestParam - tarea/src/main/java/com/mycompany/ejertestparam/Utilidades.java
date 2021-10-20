dd34abaf6de1c5ff810a51619bb21a92605414a1
Zelechos <zelechos@hotmail.com>
2021-10-19 22:46:55 -0400

    v0.0.9

--- Testing/ejerTestParam - tarea/src/main/java/com/mycompany/ejertestparam/Utilidades.java
+++ Testing/ejerTestParam - tarea/src/main/java/com/mycompany/ejertestparam/Utilidades.java
@@ -3,14 +3,17 @@
  * @Repositorio : https://github.com/Zelechos/Pragmatic_Java
  */
 package com.mycompany.ejertestparam;
+
 public class Utilidades {
-    public String concatenar(String uno,String dos,String tres){
-        if (uno!=null && dos!=null && tres!=null) {
+
+    public String concatenar(String uno, String dos, String tres) {
+        if (uno != null && dos != null && tres != null) {
             return uno.concat(" ").concat(dos).concat(" ").concat(tres);
         }
         return null;
     }
-    public int sumaNumeros(int a, int b){
-        return a+b;
+
+    public int sumaNumeros(int a, int b) {
+        return a + b;
     }
 }
