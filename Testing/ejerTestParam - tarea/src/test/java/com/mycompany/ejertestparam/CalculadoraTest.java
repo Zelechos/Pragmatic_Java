13110df80d5524838269632429af65851c0a4c9a
Zelechos <zelechos@hotmail.com>
2021-10-21 12:31:38 -0400

    v0.0.8

---
+++ Testing/ejerTestParam - tarea/src/test/java/com/mycompany/ejertestparam/CalculadoraTest.java
@@ -0,0 +1,46 @@
+package com.mycompany.ejertestparam;
+
+/*
+ * @Author : Pragmatic Coder
+ * @Repositorio : https://github.com/Zelechos/Pragmatic_Java
+ */
+import java.util.List;
+import org.assertj.core.util.Arrays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class CalculadoraTest {
+
+@Parameterized.Parameters
+    public static List<Object> datos() {
+        //devolvera una lista
+        return Arrays.asList(new Object[][]{
+            {5, 4}, {4, 6}, {3, 0}, {7, -5}
+        });
+    }
+
+    @Parameterized.Parameter(0)
+    public int valor1;
+    @Parameterized.Parameter(1)
+    public int valor2;
+    Calculadora utils = new Calculadora();
+
+    @Test
+    public void testCalculadora() {
+        System.out.println("los parametros son v1=" + valor1 + " v2=" + valor2);
+        int resultado1 = utils.suma(valor1, valor2);
+        int resultado2 = utils.resta(valor1, valor2);
+        int resultado3 = utils.multimplicacion(valor1, valor2);
+        int resultado4 = utils.division(valor1, valor2);
+
+        assertEquals(valor1 + valor2, resultado1);
+        assertEquals((valor1) - (valor2), resultado2);
+        assertEquals(valor1 * valor2, resultado3);
+        assertEquals(valor1 / valor2, resultado4);
+
+    }
+
+}
