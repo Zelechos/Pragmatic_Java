6d77358fe3821604778b6f42cbc1c172de28c2ac
Pragmatic Coder <zelechos@hotmail.com>
2022-05-16 18:06:22 -0400

    v0.0.8

---
+++ Basics/TestColoringConsole/src/testcoloringconsole/TestColoringConsole.java
@@ -0,0 +1,15 @@
+/**
+ * Class to execute all the methods of the ColoringConsole script
+ * @author: Pragmatic Coder
+ * @repository: https://github.com/Zelechos/ColoringConsole
+ */
+package testcoloringconsole;
+
+public class TestColoringConsole {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
