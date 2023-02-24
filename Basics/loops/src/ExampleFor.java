46e9a6fcbc22b9b9866698a706b44bac47e2fa80
Alex T. H <zelechos@hotmail.com>
2023-02-24 07:33:15 -0400

    feat: create loops directory

---
+++ Basics/loops/src/ExampleFor.java
@@ -0,0 +1,10 @@
+public class ExampleFor {
+
+    public static void main(String[] args) {
+
+//        Se requiere generar numero del 1 al 5
+        for (var i = 1; i < 6 ; i++) {
+            System.out.println("the index for is\t"+i);
+        }
+    }
+}
