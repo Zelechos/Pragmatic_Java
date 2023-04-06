aea8d4970e56bae4f4ef034913d9daed6e970f5a
Alex T.H <zelechos@hotmail.com>
2023-04-05 22:27:38 -0400

    v0.2.1

--- Basics/exception_handling/src/exercise_01/BasicExceptionSyntax.java
+++ Basics/exception_handling/src/exercise_01/BasicExceptionSyntax.java
@@ -7,10 +7,17 @@ public class BasicExceptionSyntax {
         int result = 0;
 
         try {
-            result = Arithmetic.division(10,0);
-        } catch (Exception e) {
-            System.out.println("Ocurrio una excepcion");
+            result = Arithmetic.division(10, 0);
+        } catch(ExceptionOperation e){
+            System.out.println("Ocurrio una excepcion de tipo ExceptionOperation");
             e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ocurrio una excepcion de tipo Exception");
+            e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("se proceso la division correctamente");
         }
         System.out.println(result);
     }
--- Basics/exception_handling/src/exercise_01/ExceptionOperation.java
+++ Basics/exception_handling/src/exercise_01/ExceptionOperation.java
@@ -1,6 +1,6 @@
 package exercise_01;
 
-public class ExceptionOperation extends Exception {
+public class ExceptionOperation extends Exception { // en la actualidad se recomienda usar la clas RuntimeExcepti on
 
     public ExceptionOperation(String message) {
         super(message);
