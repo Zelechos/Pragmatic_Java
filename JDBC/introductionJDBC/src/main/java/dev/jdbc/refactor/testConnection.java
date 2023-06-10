8fe763ad3ba48f3ec30975bf9ae31a7f9bddfe7d
Alex T.H <zelechos@hotmail.com>
2023-06-10 14:19:18 -0400

    v0.3.4

--- JDBC/introductionJDBC/src/main/java/dev/jdbc/refactor/testConnection.java
+++ JDBC/introductionJDBC/src/main/java/dev/jdbc/refactor/testConnection.java
@@ -1,58 +1,42 @@
 package dev.jdbc.refactor;
 
-import dev.jdbc.domain.Person;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class testConnection {
 
     public static void main(String[] args) throws SQLException {
 
-//        Connection connection = StandardConnection.getConnection("test");
-//        Statement statement = connection.createStatement();
-//        var sql = "SELECT * FROM person";
-//        var insertSql = "INSERT INTO test.person(name, lastname, email, phone) VALUES('JavaEE', 'JPA', 'jpa@parallel.dev', '21212121') ";
+        Connection connection = StandardConnection.getConnection("test");
+        Statement statement = connection.createStatement();
+        var sql = "SELECT * FROM person";
+        var insertSql = "INSERT INTO test.person(name, lastname, email, phone) VALUES('Alan', 'Turing', 'alant@parallel.dev', '394237924') ";
 
 //        var sql = "SELECT * FROM language";
 //        var insertSql = "INSERT INTO technologies.language(name, date) VALUES('JavaScript','1998-09-10') ";
 
-//        statement.execute(insertSql);
-//        ResultSet resultSet = statement.executeQuery(sql);
+        statement.execute(insertSql);
+        ResultSet resultSet = statement.executeQuery(sql);
 
-//        while (resultSet.next()) {
+
+        while (resultSet.next()) {
+            System.out.println("{ \nId Person : " + resultSet.getInt("id_person"));
+            System.out.println("Name : " + resultSet.getString("name"));
+            System.out.println("LastName : " + resultSet.getString("lastname"));
+            System.out.println("Email : " + resultSet.getString("email"));
+            System.out.println("Phone : " + resultSet.getString("phone") + "\n }");
+            System.out.println("---------------------------------------------------------");
 //            System.out.println("Id Language : " + resultSet.getInt("id_person"));
 //            System.out.println("Name : " + resultSet.getString("name"));
-////            System.out.println("Date of Creation : " + resultSet.getDate("date"));
+//            System.out.println("Date of Creation : " + resultSet.getDate("date"));
 //            System.out.println("---------------------------------------------------------");
-//        }
+        }
 
-//        StandardConnection.close(resultSet);
-//        StandardConnection.close(connection);
-//        StandardConnection.close(statement);
-
-
-//        DAO --> Data Access Object
-        PersonDAO dao = new PersonDAO();
-        Person person = new Person("Cyber", "Punk", "2999", "3542374395");
-        Person person1 = new Person();
-        person1.setEmail("cyberpunk@reality.cyber");
-        Person person2 = new Person();
-        person2.setEmail("swartsz@reality.punk");
-
-        dao.save(person); // to insert person in the DB
-
-        dao.getAll().forEach(System.out::println);// to get all persons of DB
-
-        System.out.println(dao.getPerson(8));
-        System.out.println(dao.getPerson(11));
-
-        dao.put(8, person1);
-        dao.put(11, person2);
-
-        System.out.println(dao.getPerson(8));
-        System.out.println(dao.getPerson(11));
-
-        dao.delete(6);
+        StandardConnection.close(resultSet);
+        StandardConnection.close(connection);
+        StandardConnection.close(statement);
 
     }
 }
