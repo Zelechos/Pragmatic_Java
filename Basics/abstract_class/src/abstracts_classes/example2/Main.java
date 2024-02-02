aaa9b855785c0002d06b8c22a8cb4508dea48d7e
Alex T.H. <zelechos@hotmail.com>
2024-02-02 14:44:49 -0400

    fix(v0.4.0): fix some files

---
+++ Basics/abstract_class/src/abstracts_classes/example2/Main.java
@@ -0,0 +1,13 @@
+package abstracts_classes.example2;
+/**
+ * Practice Abstracts classes
+ *
+ * @author Alex T.H.
+ * @version v0.3.9
+ * @since 20.0.0 2024-02-02
+ */
+public class Main {
+    public static void main(String[] args) {
+        // some code ...
+    }
+}
