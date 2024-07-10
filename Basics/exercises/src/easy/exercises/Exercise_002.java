b65a6071ff1230b7e27b21a41a18e786dd0287ca
AlexT.H. <zelechos@hotmail.com>
2024-07-10 14:39:22 -0400

    fix(v0.4.7): add changes exercises

--- Basics/exercises/src/easy/exercises/Exercise_002.java
+++ Basics/exercises/src/easy/exercises/Exercise_002.java
@@ -6,15 +6,22 @@ public class Exercise_002 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+//        System.out.println("Hello world!");
+//        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
 //        Transformacion de Streams Primitivos a Stream de Objetos para posteriormente utilizarlo para realizar el formateo
-        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).boxed().toArray(Integer[]::new));
-        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new));
-        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).mapToObj(Double::valueOf).toArray(Double[]::new));
-        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).mapToObj(Long::valueOf).toArray(Long[]::new));
+//        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).boxed().toArray(Integer[]::new));
+//        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new));
+//        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).mapToObj(Double::valueOf).toArray(Double[]::new));
+//        System.out.printf("(%s%s%s) %s%s%s-%s%s%s%s%n", Arrays.stream(numbers).mapToObj(Long::valueOf).toArray(Long[]::new));
+//
 
+        int pixels = 157;
+        System.out.println(pixelsToPoint(pixels));
 
+    }
+
+    public static double pixelsToPoint(int pixels){
+       return (pixels - 5) / 7;
     }
 }
